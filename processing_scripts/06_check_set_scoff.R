f5bbbf15e020c1670e1e299d712997d6873b75ec
Josh Hauer <hauerjosh@gmail.com>
2026-06-18 10:23:43 +0200

    create temp file path for writing new raster to same file

--- processing_scripts/06_check_set_scoff.R
+++ processing_scripts/06_check_set_scoff.R
@@ -13,21 +13,25 @@ transfer_dir <- "/mnt/ibb_share/zurell_transfer/Hauer_BMIP_data/" # for cluster
 # transfer_dir <- "//NAS-2-P-SN-01.ibb.uni-potsdam.de/daten$/AG26/Transfer/Hauer_BMIP_data/" # for testing on Windows PC
 # transfer_dir <- "//mnt/local_chelsa02" testing on Linux local mount
 
-## specify input directories
-# mask_dir <- file.path(transfer_dir, "mask_files") 
+# processed files 
 file_dir <- file.path(transfer_dir, "GEOBON_results") # splits into regions next
-
+# original chelsa files
 chelsa_dir <- file.path(transfer_dir, "CHELSA_downloads")
 
 
-## general output directory
-out_dir <- file.path(transfer_dir, "processing_extra_out") # it diversifies later in loop
+# ## general output directory
+# out_dir <- file.path(transfer_dir, "processing_extra_out") # it diversifies later in loop
 
-## Set arguments---
-mthds <-c("daily_10km", "monthly_1km")
-n_cores <- 15
+## Set arguments ----
+mthds <- c("daily_10km", "monthly_1km")
+n_cores <- 10
 
-# PROCESSED FILES -----------------------------------------------------------------------
+
+# LIST ALL FILES  ---------------------------------------------------------
+
+
+## processed files -----------------------------------------------
+
 ## list all files
 processed_files <- list.files(file.path(file_dir), pattern = ".tif", 
                         full.names = TRUE, recursive = T)
@@ -38,7 +42,8 @@ df_processed_files <- stringr::str_split(processed_files, "/", simplify = TRUE) 
   as_tibble() 
 
 # select necessary columns only
-df_processed_files <- df_processed_files[, seq(ncol(df_processed_files)-5, ncol(df_processed_files), 1)] 
+df_processed_files <- df_processed_files[, seq(ncol(df_processed_files) - 5,
+                                               ncol(df_processed_files), 1)] 
 
 # set column names
 names(df_processed_files) <- c("region_folder", "type", "sphere", 
@@ -57,9 +62,9 @@ df_processed_files <- df_processed_files %>%
 
 
 
-# CHELSA_Files ------------------------------------------------------------------------------
+## CHELSA files --------------------------------------------------
 
-# list all files ------------------------------
+# list all files 
 chelsa_files <- list.files(file.path(chelsa_dir), pattern = ".tif", 
                         full.names = TRUE, recursive = T)
 
@@ -69,7 +74,8 @@ df_chelsa_files <- stringr::str_split(chelsa_files, "/", simplify = TRUE) %>%
   as_tibble() 
 
 # select necessary columns only
-df_chelsa_files <- df_chelsa_files[, seq(ncol(df_chelsa_files)-2, ncol(df_chelsa_files), 1)] 
+df_chelsa_files <- df_chelsa_files[, seq(ncol(df_chelsa_files) - 2, 
+                                         ncol(df_chelsa_files), 1)] 
 
 # set column names
 names(df_chelsa_files) <- c("variable", "year", "file_name")
@@ -86,13 +92,12 @@ df_chelsa_files <- df_chelsa_files %>%
 # extract full path information
 df_chelsa_files$full_path <- chelsa_files
 
-# EXTRACT SCOFF per variable ---------------------------------------------------------------
+# EXTRACT CHELSA SCOFF per variable --------------------------------------------
 
-# CHELSA -----------------
 # empty list
 scoffs_chelsa <- list()
 
-#  extract variable names from chelsa
+#  extract variable names from chelsa files
 chelsa_vars <- unique(df_chelsa_files$variable)
 
 # loop over variable name idents
@@ -116,6 +121,7 @@ for (i in seq_along(chelsa_vars)) {
 
 
 # PARALLELIZE --------------------------------------------------------
+
 ## split full data in even chunks to be send to the workers -----
 l <- dim(df_processed_files)[1] # length of all processed files
 x <- round(l/n_cores) # chunk size of tasks send to each worker
@@ -124,22 +130,26 @@ to <- lead(from)-1
 idx_df <- data.frame(from,to)
 idx_df$to[n_cores] <- l
 
-if(dim(idx_df)[1] > n_cores)idx_df <- idx_df[-(n_cores+1),]
+if (dim(idx_df)[1] > n_cores) {
+  idx_df <- idx_df[-(n_cores + 1),]
+}
 
-
+# make cluster
 cl <- makeCluster(n_cores)
 registerDoParallel(cl)
 
-# idx_df
 
-# start parallel loop
-foo <- foreach(i = 1:n_cores, .packages = c("terra", "tidyverse"),.combine = c) %dopar% {
+## start parallel loop ----
+foo <- foreach(i = 1:n_cores, 
+               .packages = c("terra", "tidyverse"),
+               .combine = c) %dopar% {
   
   # make chunk
   df_processed_sub <- df_processed_files %>% 
     slice(idx_df$from[i]:idx_df$to[i])
   
-  cat("\nWorker", i, "processing", nrow(df_processed_sub), "files from", nrow(df_processed_files), "total files...\n")
+  cat("\nWorker", i, "processing", nrow(df_processed_sub), 
+      "files from", nrow(df_processed_files), "total files...\n")
 
 
   # loop  over each row in df_processed_sub
@@ -158,17 +168,30 @@ foo <- foreach(i = 1:n_cores, .packages = c("terra", "tidyverse"),.combine = c) 
       next
     }
     
-    # read in file
-    r_processed <- terra::rast(file_path_j)
+    # create tmp file path
+    tmp_path <- gsub(basename(file_path_j), 
+                     paste0("tmp_", basename(file_path_j)),
+                     file_path_j)
+    
+    # rename original file into temporary file name
+    file.rename(file_path_j, tmp_path)
+    
+    # read in tmp file
+    r_processed <- terra::rast(tmp_path)
     
     # apply scoff from CHELSA
     terra::scoff(r_processed) <- scoff_j
     
-    # write back to same file
+    
+    # write back to original file name, but set new scoff
     terra::writeRaster(r_processed, file_path_j, overwrite = TRUE,
                        # expliciltly set scoff values when writing to file again!!
                        scale  = scoff_j[, 1], 
                        offset = scoff_j[, 2])
+    
+    # delete temporary file
+    file.remove(tmp_path)
+    
   } # close loop over files
 } # close parallel loop
   
@@ -180,57 +203,51 @@ foo <- foreach(i = 1:n_cores, .packages = c("terra", "tidyverse"),.combine = c) 
 
 
 
-# EXTRA -----
-# # processed files -----------
-# # empty list
-# scoffs_processed <- list()
+# # TESTING -----------------------------------------------------------------
 # 
-# # extract variable and method names of processed files
-# processed_vars <- unique(df_processed_files$variable)
-# processed_mthds <- unique(df_processed_files$method)
 # 
-# # loop over methods
-# for (i in seq_along(processed_mthds)) {
-#   # filter for all files of that method
-#   df_mthd_files <- df_processed_files %>% 
-#     filter(method == processed_mthds[i]) 
-#   
-#   # create empty sublist
-#   sublist <- list()
-#   
-#   # loop over variables in that method
-#   for (j in seq_along(processed_vars)) {
-#     # extract first .tif file  of variable~method combinations
-#     var_file <- df_mthd_files %>% 
-#       filter(variable == processed_vars[j]) %>% 
-#       pull(full_path) %>% 
-#       first()
-#     
-#     # read in raster
-#     r_processed_var <- terra::rast(var_file)
-#     
-#     # extract scoff matrix and store in sublist
-#     sublist[[j]] <- terra::scoff(r_processed_var[[1]])
-#   
-#     # set sublist name to variable name
-#     names(sublist)[[j]] <- processed_vars[j]
-#   } # close loop  over variables
-#   
-#   # add scoff matrix to final list of processed files
-#   scoffs_processed[[i]] <- sublist
-#   # set method as name of final list
-#   names(scoffs_processed)[[i]] <- processed_mthds[i]
-# } # close loop over methods
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # extract single file path
+# file_path_j <- df_processed_files$full_path[1]
+# 
+# # variable and scoff extraction
+# var_j <- df_processed_files$variable[1]
+# scoff_j <- scoffs_chelsa[[var_j]]
+# 
+# # create tmp file
+# tmp_path <- gsub(basename(file_path_j), 
+#                  paste0("tmp_", basename(file_path_j)),
+#                  file_path_j)
+# 
+# file.rename(file_path_j, tmp_path)
+# 
+# # read  single file
+# r_processed <- terra::rast(tmp_path)
+# 
+# # see scoff
+# scoff(r_processed)[1,]
+# 
+# # apply scoff chelsa
+# scoff(r_processed) <- scoff_j
+# 
+# 
+# # Write new raster to original filename
+# writeRaster(
+#   r_processed,
+#   file_path_j,
+#   overwrite = TRUE,
+#   scale  = scoff_j[, 1],
+#   offset = scoff_j[, 2]
+# )
+# 
+# # Remove backup
+# if (file.exists(file_path_j)) {
+#   file.remove(tmp_path)
+# }
+# 
+# # Verify file existance and scoff set
+# r_new <- rast(file_path_j)
+# scoff_new <- scoff(r_new)
+# 
+# cat("Original file now has scoff:\n")
+# print(scoff_new)
+# cat("Backup file exists:", file.exists(tmp_path), "\n")
